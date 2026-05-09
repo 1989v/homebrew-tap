ecec11105ae887030e11e471fdab31d175bdec59
kwongd <gideok.kwon@myrealtrip.com>
2026-05-09 12:24:43 +0900

    muxbar: 0.2.0

--- Casks/muxbar.rb
+++ Casks/muxbar.rb
@@ -1,20 +1,19 @@
 cask "muxbar" do
-  version "0.1.0"
-  sha256 "f5984fe7fd8cf205756967c4376e723f5dc133e80ab85c692cbb214bbbebdfa1"
+  version "0.2.0"
+  sha256 "fbbd4583afd61836e04909d1181159ca8f3bfee49ec8322834171aef5638616f"
 
   url "https://github.com/1989v/muxbar/releases/download/v#{version}/muxbar-#{version}.dmg"
   name "muxbar"
-  desc "tmux session manager with caffeinate toggle in the menu bar"
+  desc "tmux session manager + closed-lid mode in the menu bar"
   homepage "https://github.com/1989v/muxbar"
 
   depends_on formula: "tmux"
   depends_on macos: ">= :ventura"
-  depends_on arch: :arm64
 
   app "muxbar.app"
 
   postflight do
-    system_command "/usr/bin/xattr",
+    system_command "xattr",
                    args: ["-dr", "com.apple.quarantine", "#{appdir}/muxbar.app"],
                    sudo: false
   end
